606dfc37ba265184c608a31ab862d9b74583a386
Abdullahi <benyusouf>
2021-08-10 01:35:26 +0100

    Added TestApi

---
+++ FacesApiTest/ImageUtility.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace FacesApiTest
+{
+    public class ImageUtility
+    {
+        public byte[] ConvertToBytes(string imagePath)
+        {
+            var ms = new MemoryStream();
+
+            using var stream = new FileStream(imagePath, FileMode.Open);
+
+            stream.CopyTo(ms);
+
+            return ms.ToArray();
+        }
+
+        public void ConvertToImage(byte[] imageBytes, string fileName)
+        {
+            using var ms = new MemoryStream();
+            var image = Image.FromStream(ms);
+            image.Save(fileName + ".jpg");
+        }
+    }
+}
---
+++ FacesApiTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace FacesApiTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var imagePath = @"oscars-2017.jpg";
+            var endpointUrl = "http://localhost:5000/api/faces";
+            var imageUtil = new ImageUtility();
+            var bytes = imageUtil.ConvertToBytes(imagePath);
+            List<byte[]> faceList = null;
+            var byteContent = new ByteArrayContent(bytes);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.PostAsync(endpointUrl, byteContent);
+            var apiResponse = await response.Content.ReadAsStringAsync();
+
+            faceList = JsonConvert.DeserializeObject<List<byte[]>>(apiResponse);
+
+            if(faceList.Count > 0)
+            {
+                for (int i = 0; i < faceList.Count; i++)
+                {
+                    imageUtil.ConvertToImage(faceList[i], "face" + i);
+                }
+            }
+        }
+    }
+}
